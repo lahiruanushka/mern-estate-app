99bcc3050a03f4c1a475ceed1e8d2c5dbf4a6944
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-14 17:05:06 +0530

    Create User model

---
+++ api/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+// define the schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true, // Trims any extra spaces
+      minlength: [3, "Username must be at least 3 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      match: [/\S+@\S+\.\S+/, "Please use a valid email address"], // Regex to validate email format
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+  },
+  { timestamps: true }
+);
+
+// Create the model
+const User = mongoose.model("User", userSchema);
+
+export default User;
