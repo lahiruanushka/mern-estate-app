9ef3910903c55a3ed8df4c7a876aac9a736a1644
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-25 09:22:48 +0530

    Change the default image

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -24,8 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     avatar: {
       type: String,
-      default:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+      default: "https://cdn-icons-png.flaticon.com/128/9187/9187604.png",
     },
   },
   { timestamps: true }
