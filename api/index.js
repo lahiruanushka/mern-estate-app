d8447c1e5dd38db3df950f31b12312b17b0b2c87
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-12-11 10:08:18 +0530

    feat: serve frontend assets and handle undefined routes
    - Configured `express.static` to serve frontend assets from `/frontend/dist`.
    - Added wildcard route to serve `index.html` for client-side routing.
    - Implemented middleware to handle undefined routes with a 404 status and JSON response.

--- api/index.js
+++ api/index.js
@@ -23,6 +23,17 @@ app.use("/api/user", userRotes);
 app.use("/api/auth", authRotes);
 app.use('/api/listings', listingRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+// Handle undefined routes
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
 // Middlewares
 app.use(errorHandler);
 
